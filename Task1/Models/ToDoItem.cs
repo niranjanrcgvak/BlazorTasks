35c5cf6aff559971f188a4317fc40bdd5bb9f094
niranjan <niranjan@cgvakindia.com>
2024-09-29 23:06:11 +0530

    Task 1 changes

---
+++ Task1/Models/ToDoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1.Models
+{
+    public class ToDoItem
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsEditing { get; set; }
+    }
+}
